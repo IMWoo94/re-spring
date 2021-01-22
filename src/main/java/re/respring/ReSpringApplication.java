f681e00cacaeab40c572103489fd0c1ead5b025a
unknown <dlrldyd1002@naver.com>
2021-01-22 21:34:10 +0900

    Initial commit

---
+++ src/main/java/re/respring/ReSpringApplication.java
@@ -0,0 +1,13 @@
+package re.respring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReSpringApplication.class, args);
+	}
+
+}
